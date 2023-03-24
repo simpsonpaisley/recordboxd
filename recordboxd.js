bb9b043b603c419ce6bd47f79b52e06df9a675fc
simpsonpaisley <simpsonpaisley@gmail.com>
2023-03-24 21:24:40 +0000

    Added functionality to render album data on 'History' page. At the moment, after refreshing the page, the data overwrites the older data

--- recordboxd.js
+++ recordboxd.js
@@ -1,5 +1,15 @@
-// Script to take user to "Add Album Page when "Get Started" Button is clicked
+'use strict';
 
+// Script to stop array being overwritten each time the form is submitted.
+
+let allAlbums = []; // array for storing form data.
+
+let savedAlbums = localStorage.getItem('albumArray');
+if (savedAlbums) {
+	allAlbums = JSON.parse(savedAlbums);
+}
+
+// Script to take user to "Add Album Page when "Get Started" Button is clicked
 function getStartedHandler() {
 	window.location.href = 'addAlbum.html';
 }
@@ -41,28 +51,104 @@ function addTrackHandler() {
 }
 
 // Form Submit
-let allAlbums = []; // array for storing form data.
-let albumNumber = 0; // for album numbers
+
+//let clickNumber = 0;
+let indexNumber = 0;
 function formSubmit() {
 	event.preventDefault(); // Stops Page From refreshing on submit.
-	albumNumber++;
-
+	//clickNumber++;
 	let formElement = document.getElementById('addAlbumForm'); // Getting Form Element
 
-	let userAlbums = new FormData(formElement); // Creating a new object
+	let userAlbums = new FormData(formElement); // Creating a new object.
 
-	let albumData = Object.fromEntries(userAlbums.entries()); //
+	let albumData = Object.fromEntries(userAlbums.entries()); //Populating the object.
 
-	allAlbums.push(albumData);
+	allAlbums[indexNumber] = albumData;
 
-	localStorage.setItem('albumArray', JSON.stringify(allAlbums));
+	//allAlbums.push(albumData); // Adding object to array.
+	indexNumber++;
+	localStorage.setItem('albumArray', JSON.stringify(allAlbums)); // converting array to string, and then pushing to local storage.
+
+	//console.log(allAlbums);
+
+	document.getElementById('addAlbumForm').reset(); // Clears previous information from inputs when form is submitted.
+}
+
+// Unstringifying array
+
+// To Display Album Information on Cards.
+
+if (allAlbums.length > 0) {
+	let displayAlbumInfo = localStorage.getItem('albumArray'); //Getting Array back from Local Server.
+
+	let allAlbumsParsed = JSON.parse(displayAlbumInfo); //Parsing stringified array into a new array.
 
 	console.log(allAlbums);
-	/* i = 0;
 
-	for (i; i < trackNumber; i++) {
-		console.log(i);
+	for (let j = 0; j < allAlbumsParsed.length; j++) {
+		//Loop through each index of the array.
+		let cardSpace = document.getElementById('albumCardSpace'); //Getting card space area.
+
+		let cardDiv = document.createElement('div'); //Creating Card Div.
+		cardDiv.setAttribute('class', 'albumCard'); // Setting class for styling.
+
+		// Adding Image
+		let cardImg = document.createElement('img'); //Creating Img Element
+		let imgSrc = allAlbumsParsed[j].albumArt; // Getting Image SRC from Object.
+		cardImg.setAttribute('src', imgSrc); // Adding src attrivute and link.
+		cardImg.setAttribute('class', 'albumArtDisplay'); // Setting class for styling.
+		cardImg.setAttribute('alt', 'album art'); // Adding alt text.
+
+		// Adding Album Title Header
+
+		let cardHeader = document.createElement('h3');
+		let headerText = allAlbumsParsed[j].albumName;
+		cardHeader.setAttribute('class', 'albumTitle');
+		cardHeader.innerText = headerText;
+
+		// Adding Info Line 1, containing Artist and Release Year.
+
+		let albumInfo1 = document.createElement('div');
+		albumInfo1.setAttribute('class', 'albumInfoLine1');
+
+		let artistDisplay = document.createElement('p');
+		artistDisplay.setAttribute('class', 'albumInfoPara');
+		let artistText = allAlbumsParsed[j].albumArtist;
+		artistDisplay.innerText = artistText;
+
+		let yearDisplay = document.createElement('p');
+		yearDisplay.setAttribute('class', 'albumInfoPara');
+		let yearText = allAlbumsParsed[j].releaseYear;
+		yearDisplay.innerText = yearText;
+
+		albumInfo1.appendChild(artistDisplay);
+		albumInfo1.appendChild(yearDisplay);
+
+		// Adding Ingo Line 2, containing rating and button.
+
+		let albumInfo2 = document.createElement('div');
+		albumInfo2.setAttribute('class', 'albumInfoLine2');
+
+		let ratingDisplay = document.createElement('p');
+		let ratingText = allAlbumsParsed[j].rating;
+		ratingDisplay.innerText = ratingText;
+
+		let buttonDisplay = document.createElement('button');
+		buttonDisplay.setAttribute('class', 'seeMore');
+		buttonDisplay.innerText = 'See More';
+
+		albumInfo2.appendChild(ratingDisplay);
+		albumInfo2.appendChild(buttonDisplay);
+
+		// Append Everything to card div
+
+		cardDiv.appendChild(cardImg);
+		cardDiv.appendChild(cardHeader);
+		cardDiv.appendChild(albumInfo1);
+		cardDiv.appendChild(albumInfo2);
+
+		// Append Card Div to Card Space
+
+		cardSpace.appendChild(cardDiv);
 	}
-*/
-	document.getElementById('addAlbumForm').reset(); // Clears previous information from inputs when form is submitted.
 }
---
+++ savedCode.md
@@ -0,0 +1,52 @@
+````html
+<div class="albumCard">
+	<img
+		src="/images/coverpng.png"
+		alt="album art"
+		class="albumArtDisplay" />
+	<h3 class="albumTitle">Album Title</h3>
+	<div class="albumInfoLine1">
+		<p class="albumInfoPara">Album Artist</p>
+		<p class="albumInfoPara">Release Year</p>
+	</div>
+
+	<div class="albumInfoLine2">
+		<p>Rating</p>
+		<button class="seeMore">See More</button>
+	</div>
+</div>
+<div class="albumCard">
+	<img
+		src="/images/coverpng.png"
+		alt="album art"
+		class="albumArtDisplay" />
+	<h3 class="albumTitle">Album Title</h3>
+	<div class="albumInfoLine1">
+		<p class="albumInfoPara">Album Artist</p>
+		<p class="albumInfoPara">Release Year</p>
+	</div>
+
+	<div class="albumInfoLine2">
+		<p>Rating</p>
+		<button class="seeMore">See More</button>
+	</div>
+</div>
+<div class="albumCard">
+	<img
+		src="/images/coverpng.png"
+		alt="album art"
+		class="albumArtDisplay" />
+	<h3 class="albumTitle">Album Title</h3>
+	<div class="albumInfoLine1">
+		<p class="albumInfoPara">Album Artist</p>
+		<p class="albumInfoPara">Release Year</p>
+	</div>
+
+	<div class="albumInfoLine2">
+		<p>Rating</p>
+		<button class="seeMore">See More</button>
+	</div>
+</div>
+
+```
+````
